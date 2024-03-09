99e3f9692e5de0e99ee4830b4468d2a601024c54
yungshun317 <seraph317@gmail.com>
2024-03-09 12:23:48 +0800

    [FEAT] Use short-circuit evaluation for displaying the steps component

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ export default function App() {
     [[Prototype]]: Array(0)
   */
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
       if (step > 1) setStep(step - 1);
@@ -27,20 +28,26 @@ export default function App() {
   }
 
   return (
-      <div className="steps">
-        <div className="numbers">
-          <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-          <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-          <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-        </div>
+      <>
+          <button className="close" onClick={() => setIsOpen(!isOpen)}>&times;</button>
 
-        <p className="message">Step {step}: {messages[step - 1]}</p>
+          {isOpen && (
+              <div className="steps">
+                  <div className="numbers">
+                      <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+                      <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+                      <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+                  </div>
 
-          <div className="buttons">
-              {/* <button className="previous" onClick={() => alert("Previous")}>Previous</button> */}
-              <button className="previous" onClick={handlePrevious}>Previous</button>
-              <button className="next" onClick={handleNext}>Next</button>
-          </div>
-      </div>
+                  <p className="message">Step {step}: {messages[step - 1]}</p>
+
+                  <div className="buttons">
+                      {/* <button className="previous" onClick={() => alert("Previous")}>Previous</button> */}
+                      <button className="previous" onClick={handlePrevious}>Previous</button>
+                      <button className="next" onClick={handleNext}>Next</button>
+                  </div>
+              </div>
+          )}
+      </>
   )
 }
