25b9435195a78bd7b216c783545a62f20df1ff46
yungshun317 <seraph317@gmail.com>
2024-03-09 12:31:20 +0800

    [FEAT] Update states based on the current states

--- src/App.js
+++ src/App.js
@@ -20,16 +20,16 @@ export default function App() {
   const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
-      if (step > 1) setStep(step - 1);
+      if (step > 1) setStep((s) => s - 1);
   }
 
   function handleNext() {
-      if (step < 3) setStep(step + 1);
+      if (step < 3) setStep((s) => s + 1);
   }
 
   return (
       <>
-          <button className="close" onClick={() => setIsOpen(!isOpen)}>&times;</button>
+          <button className="close" onClick={() => setIsOpen((is) => !is)}>&times;</button>
 
           {isOpen && (
               <div className="steps">
