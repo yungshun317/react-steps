d49f911987daad80521bde4744284046d1fc15c2
yungshun317 <seraph317@gmail.com>
2024-03-17 16:33:44 +0800

    [FEAT] Use the children prop to make the button component reusable

--- src/App.js
+++ src/App.js
@@ -42,20 +42,12 @@ export default function App() {
                   <p className="message">Step {step}: {messages[step - 1]}</p>
 
                   <div className="buttons">
-                      <Button
-                          bgColor="#7950f2"
-                          textColor="#fff"
-                          onClick={handlePrevious}
-                          text="Previous"
-                          emoji="👈"
-                      />
-                      <Button
-                          bgColor="#7950f2"
-                          textColor="#fff"
-                          onClick={handleNext}
-                          text="Next"
-                          emoji="👉"
-                      />
+                      <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
+                          <span>👈</span> Previous
+                      </Button>
+                      <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
+                          Next <span>👉</span>
+                      </Button>
                   </div>
               </div>
           )}
@@ -63,13 +55,13 @@ export default function App() {
   )
 }
 
-function Button({textColor, bgColor, onClick, text, emoji}) {
+function Button({textColor, bgColor, onClick, children}) {
     return (
         <button
             style={{ backgroundColor: bgColor, color: textColor }}
             onClick={onClick}
         >
-            <span>{emoji}</span> {text}
+            {children}
         </button>
     )
 }
